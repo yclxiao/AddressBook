b8b45464c40b2d2ee6d1ab13460ef86ef0d37b21
OF1089（杨成龙） <yangchenglong@qianmi.com>
2017-08-03 11:52:14 +0800

    Initial Commit

---
+++ AddressBook/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  AddressBook
+//
+//  Created by yclxiao on 2017/8/3.
+//  Copyright © 2017年 yclxiao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ AddressBookTests/AddressBookTests.m
@@ -0,0 +1,39 @@
+//
+//  AddressBookTests.m
+//  AddressBookTests
+//
+//  Created by yclxiao on 2017/8/3.
+//  Copyright © 2017年 yclxiao. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface AddressBookTests : XCTestCase
+
+@end
+
+@implementation AddressBookTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
